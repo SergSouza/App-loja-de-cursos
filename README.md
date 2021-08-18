9c5ee602a151eb119d2b7661e1f7933557b36ab0
sergio.escureto <sergio.escureto@gmail.com>
2021-08-18 16:51:56 -0300

    criando meu app de uma loja de curso

---
+++ README.md
@@ -0,0 +1,1 @@
+# App-loja-de-cursos
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  lista = [
+    { id: 1, titulo: "Curso de React-native", descricao: "Aprenda REACT_NATIVE na pratica", valor: 29.99, valor_txt: "R$ 29,99", imagem: "https://reactnative.dev/img/logo-og.png" },
+    { id: 2, titulo: "Curso de JavaScrip", descricao: "Aprenda JavaScrip na pratica", valor: 29.99, valor_txt: "R$ 29,99", imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"}
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
